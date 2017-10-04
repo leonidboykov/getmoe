46dd031659fa59480fda6d41887aa9adfef56de1
Leonid Boykov <lordkratos.47@gmail.com>
2017-10-04 19:42:17 +0300

    Basic CLI implemented

--- cmd/getmoe/main.go
+++ cmd/getmoe/main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/leonidboykov/getmoe"
-	"github.com/leonidboykov/getmoe/board/sankaku"
+	"github.com/leonidboykov/getmoe/board"
 )
 
 func main() {
-	// board := moebooru.YandeReConfig
-	board := sankaku.ChanSankakuConfig
-	board.BuildAuth("xxx", "xxx")
+	fromFlag := flag.String("from", "", "Source")
+	tagsFlag := flag.String("tags", "", "Tags")
+	formatFlag := flag.String("format", "image", "Save for {image|json}")
+	saveFlag := flag.String("save", "", "Save directory")
+	loginFlag := flag.String("login", "", "Login")
+	passwordFlag := flag.String("password", "", "Password")
+	flag.Parse()
+
+	if *fromFlag == "" || *formatFlag == "" || *saveFlag == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	board, ok := board.AvailableBoards[*fromFlag]
+	if !ok {
+		fmt.Printf("There are no %s source specified\n", *fromFlag)
+		os.Exit(1)
+	}
+
+	if *loginFlag != "" && *passwordFlag != "" {
+		board.BuildAuth(*loginFlag, *passwordFlag)
+	}
 
 	board.Query = getmoe.Query{
-		Tags: []string{"nipples", "rating:e"},
+		Tags: strings.Split(*tagsFlag, " "),
 		Page: 1,
 	}
 
