01e1ac8dd2a0170f0afd86cf14a9f15ade8b7813
Leonid Boykov <lordkratos.47@gmail.com>
2017-10-06 00:23:32 +0300

    Added Gelbooru and Danbooru Post structs

--- board/gelbooru/post.go
+++ board/gelbooru/post.go
@@ -1,1 +1,21 @@
 package gelbooru
+
+// Post contains native Gelbooru data
+type Post struct {
+	Directory    string      `json:"directory"`
+	Hash         string      `json:"hash"`
+	Height       int         `json:"height"`
+	ID           int         `json:"id"`
+	Image        string      `json:"image"`
+	Change       int         `json:"change"`
+	Owner        string      `json:"owner"`
+	ParentID     interface{} `json:"parent_id"`
+	Rating       string      `json:"rating"`
+	Sample       bool        `json:"sample"`
+	SampleHeight int         `json:"sample_height"`
+	SampleWidth  int         `json:"sample_width"`
+	Score        int         `json:"score"`
+	Tags         string      `json:"tags"`
+	Width        int         `json:"width"`
+	FileURL      string      `json:"file_url"`
+}
