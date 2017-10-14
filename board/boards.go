ba99a69f14b5e708844d4fda50277c2b3268ecd6
Leonid Boykov <lordkratos.47@gmail.com>
2017-10-14 19:50:29 +0300

    Added preconfigured boards to the list of available boards

--- board/boards.go
+++ board/boards.go
@@ -2,6 +2,8 @@ package board
 
 import (
 	"github.com/leonidboykov/getmoe"
+	"github.com/leonidboykov/getmoe/board/danbooru"
+	"github.com/leonidboykov/getmoe/board/gelbooru"
 	"github.com/leonidboykov/getmoe/board/moebooru"
 	"github.com/leonidboykov/getmoe/board/sankaku"
 )
@@ -10,6 +12,8 @@ import (
 var AvailableBoards = map[string]getmoe.Board{
 	"yande.re":                moebooru.YandeReConfig,
 	"konachan.com":            moebooru.KonachanConfig,
+	"gelbooru.com":            gelbooru.GelbooruConfig,
+	"danbooru.donmai.us":      danbooru.DanbooruDonmaiUsConfig,
 	"chan.sankakucomplex.com": sankaku.ChanSankakuConfig,
 	"idol.sankakucomplex.com": sankaku.IdolSankakuConfig,
 }
